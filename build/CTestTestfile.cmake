7026b883bc20a2e3dc9342a6c84f31c283341d88
khhandrea <khhandrea@kakao.com>
2023-12-06 13:07:01 +0900

    feat: Remove build file

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /volume
-# Build directory: /volume/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/volume/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
